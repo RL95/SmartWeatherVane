bd494c90ce5591518c922848a562bdc4da533daf
RL95 <ruiliang.lin@gmail.com>
2023-06-13 10:11:56 +0200

    added initial motion profiling files

---
+++ Firmware/Core/Src/Drivers/LUT_jerk_limited_profile.h
@@ -0,0 +1,13 @@
+/*
+ * LUT_jerk_limited_profile.h
+ *
+ *  Created on: June 12, 2023
+ *      Author: R.Lin
+ */
+
+#ifndef SRC_DRIVERS_LUT_JERK_LIMITED_PROFILE_H_
+#define SRC_DRIVERS_LUT_JERK_LIMITED_PROFILE_H_
+
+
+
+#endif /* SRC_DRIVERS_LUT_JERK_LIMITED_PROFILE_H_ */
